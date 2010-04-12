ebab3d8b63a93467a8ecb5c4a36ef6934f517f14
mpd <michael@tennmandigital.com>
2010-04-12 13:07:58 -0700

    refactor

--- cpu/cpu_6502.rb
+++ cpu/cpu_6502.rb
@@ -394,61 +394,38 @@ class Cpu6502
       when 0XC5 # CMP zeropage
         @pc += 2
         address = oper1
-        set_carry(@register[:A] >= @ram[address])
-        result = (@register[:A] - @ram[address]) & 0xFF
-        set_zero(result)
-        set_sign(result)
+        op_cmp(address)
 
       when 0XD5 # CMP zeropagex
         @pc += 2
         address = oper1 + @register[:X]
         address -= 0xFF while address > 0xFF
-        set_carry(@register[:A] >= @ram[address])
-        result = (@register[:A] - @ram[address]) & 0xFF
-        set_zero(result)
-        set_sign(result)
+        op_cmp(address)
 
       when 0XCD # CMP absolute
         @pc += 3
         address = (oper1 << 8) | oper2
-        set_carry(@register[:A] >= @ram[address])
-        result = (@register[:A] - @ram[address]) & 0xFF
-        set_zero(result)
-        set_sign(result)
+        op_cmp(address)
 
       when 0XDD # CMP absolutex
         @pc += 3
         address = ((oper1 << 8) | oper2) + @register[:X]
-        set_carry(@register[:A] >= @ram[address])
-        result = (@register[:A] - @ram[address]) & 0xFF
-        set_zero(result)
-        set_sign(result)
+        op_cmp(address)
 
       when 0XD9 # CMP absolutey
         @pc += 3
         address = ((oper1 << 8) | oper2) + @register[:Y]
-        set_carry(@register[:A] >= @ram[address])
-        result = (@register[:A] - @ram[address]) & 0xFF
-        set_zero(result)
-        set_sign(result)
+        op_cmp(address)
 
       when 0XC1 # CMP indirectx
         @pc += 2
         address = indirect_x_address(oper1)
-        # address -= 0xFF while address > 0xFF
-        set_carry(@register[:A] >= @ram[address])
-        result = (@register[:A] - @ram[address]) & 0xFF
-        set_zero(result)
-        set_sign(result)
+        op_cmp(address)
 
       when 0XD1 # CMP indirecty
         @pc += 2
         address = indirect_y_address(oper1)
-        # address -= 0xFF while address > 0xFF
-        set_carry(@register[:A] >= @ram[address])
-        result = (@register[:A] - @ram[address]) & 0xFF
-        set_zero(result)
-        set_sign(result)
+        op_cmp(address)
 
       when 0xEA # NOP
         @pc += 1
@@ -537,6 +514,13 @@ class Cpu6502
     @ram[address] = (@ram[address] << 1) & 0xFF
     set_zero(@ram[address])
     set_sign(@ram[address])
+  end
+
+  def op_cmp(address)
+    set_carry(@register[:A] >= @ram[address])
+    result = (@register[:A] - @ram[address]) & 0xFF
+    set_zero(result)
+    set_sign(result)
   end
 
   def branch_pc(arg)
