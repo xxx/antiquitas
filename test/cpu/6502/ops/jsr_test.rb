79c729c43b631b0b6b9fecd9a18ead8b6cde5aad
mpd <mpd@jesters-court.net>
2010-04-13 21:52:10 -0700

    refactor

--- test/cpu/6502/ops/jsr_test.rb
+++ test/cpu/6502/ops/jsr_test.rb
@@ -7,16 +7,20 @@ class Cpu6502JsrTest < Test::Unit::TestCase
     end
 
     context "absolute mode" do
+      setup do
+        @op = 0x20
+      end
+
       should "push the current pc - 1 (after incrementing it for this op) onto the stack" do
         @cpu.pc = 2100
-        @cpu.runop(0x20, 0x40, 0x25)
+        @cpu.runop(@op, 0x40, 0x25)
         low_end = @cpu.pull
         high_end = @cpu.pull
         assert_equal 2102, (high_end << 8) | low_end
       end
 
       should "set the program counter to the passed 16-bit memory address" do
-        @cpu.runop(0x20, 0x40, 0x25)
+        @cpu.runop(@op, 0x40, 0x25)
         assert_equal to16bit(0x40, 0x25), @cpu.pc
       end
     end
